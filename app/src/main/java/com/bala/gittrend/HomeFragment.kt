c0da1d33a7b50c6552095e90ec6c8e323021a481
balu8108 <atluribalachandar8108@gmail.com>
2021-11-14 22:38:41 +0530

    create viewmodel

--- app/src/main/java/com/bala/gittrend/HomeFragment.kt
+++ app/src/main/java/com/bala/gittrend/HomeFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.bala.gittrend.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private val viewModel: HomeViewModel by viewModels()
+    private val viewBinding
+        get() = _viewBinding!!
+    private var _viewBinding: FragmentHomeBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return FragmentHomeBinding.inflate(inflater).root
+    ): View? {
+        _viewBinding = FragmentHomeBinding.inflate(inflater)
+        return _viewBinding?.root
     }
 }
---
+++ app/src/main/java/com/bala/gittrend/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.bala.gittrend
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel(val savedStateHandle: SavedStateHandle) : ViewModel() {
+}
